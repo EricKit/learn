843dd1b98f64f16a2fc814f5f01044b3b3dfcce3
Eric Kitaif <eric.kitaif@gmail.com>
2025-07-05 13:56:37 +0000

    Updated files

--- javascript-basics.js
+++ javascript-basics.js
@@ -1,10 +1,15 @@
-// This is a function. Don't worry about the format for now. It doesn't run right away.
-let addFunction = () => {
-  const firstNumber = document.getElementById("first").value;
-  const secondNumber = document.getElementById("second").value;
-  const result = parseInt(firstNumber) + parseInt(secondNumber);
-  document.getElementById("result").innerText = result;
+// This is a function. Don't worry about the format for now. It's a block of code that is executed when
+//  you tell it to execute. You will notice that the function, which starts with () => {, is stored in a
+//  variable named addFunction. We can use this variable later.
+const addFunction = () => {
+  const firstNumber = document.getElementById("first").value; // Get the first number
+  const secondNumber = document.getElementById("second").value; // Get the second number
+  const result = parseInt(firstNumber) + parseInt(secondNumber); // Convert the strings to numbers and add them
+  document.getElementById("result").innerText = result; // Set the result!
 };
 
-// Whenever the button with an ID of "add" is clicked, it runs the above function.
+// Whenever the button with an ID of "add" is clicked, it runs the named function.
 document.getElementById("add").onclick = addFunction;
+
+// You could also do this to get automatic results anytime they change the value.
+document.getElementById("first").onchange = addFunction;
